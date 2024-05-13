d8511b273eabe286be06171540da377ea5019175
Nurul Islam Molla <79696382+UXNurul@users.noreply.github.com>
2024-05-14 02:06:50 +0530

    Update index.js 2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 const app = express ( );
-const port = 9000:
-app.use(" /", (reg, res) => 1 res.json ( { message:
-"Hello From Express App" \);
+const port = 9000;
+app.use(" /", (reg, res) => {
+ res.json ({ message:
+"Hello From Express App" });
                                          });
 
-app.listen (9000, () => (
-console.log( Starting Server on Port $(port)")
+app.listen (9000, () => {
+console.log( "Starting Server on Port $(port)")
 });
